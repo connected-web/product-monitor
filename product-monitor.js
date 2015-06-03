a76221c8371560bcb25c941a858704e8a4f66717
John Beech <github@mkv25.net>
2015-06-03 18:13:52 +0100

    Moved renderMainTemplate to mainController.js, and CORS Support to corsSupport.js

--- product-monitor.js
+++ product-monitor.js
@@ -11,7 +11,6 @@ module.exports = function(config) {
   var instance = {};
   var merge = require('utils-merge');
   var url = require('url');
-  var fs = require('fs');
   var express = require('express');
 
   var config = merge({
@@ -20,92 +19,22 @@ module.exports = function(config) {
     "contentPath": "node_modules/product-monitor/monitoring/content/"
   }, config || {});
 
-  var serverPort = config.serverPort;
-  var componentsPath = config.componentsPath;
-  var contentPath = config.contentPath;
-
   // Create server
   var server = express();
 
   // Permit CORS access to this server
-  server.use(function(req, res, next) {
-    var origin = req.get('origin');
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  var corsSupport = require('./lib/monitor/corsSupport');
+  server.use(corsSupport);
 
-    next();
-  });
-
-  // Render content
-  function renderPageContent(req, res) {
-
-  	var environment = req.params.environment ? req.params.environment : 'index';
-
-  	var templatePath = componentsPath + 'page-template.html';
-  	var response = '';
-
-  	function loadComponent(name) {
-  		return fs.readFileSync(componentsPath + name);
-  	}
-
-  	function loadContent(name) {
-  		return fs.readFileSync(contentPath + name);
-  	}
-
-  	function loadComponents(path) {
-
-  		var components = [];
-  		var files = fs.readdirSync(componentsPath).filter(function(file) {
-  			return file.match(/.*component.html/);
-  		});
-
-  		components = files.map(function(file) {
-  			return loadComponent(file);
-  		});
-
-  		return components;
-  	}
-
-  	// Load page template
-  	try {
-  		var template = fs.readFileSync(templatePath, {encoding: "UTF-8"});
-  	}
-  	catch(e) {
-  		return res.status(500).send('Template file not found: ' + templatePath + '. If this is your server, please create this file.');
-  	}
-
-  	// Load components
-  	try {
-  		var components = loadComponents(componentsPath);
-  	}
-  	catch(e) {
-  		return res.status(500).send('Error loading components. If this is your server, please check that the component directory exists.');
-  	}
-
-  	// Load page content
-  	var contentFile = environment + '.content.html';
-  	try {
-  		var pageContent = loadContent(contentFile);
-  	}
-  	catch(e) {
-  		return res.status(500).send('Content file not found: ' + contentFile + '. If this is your server, please create this file.');
-  	}
-
-  	// Replace variables in template
-  	template = template.replace('${COMPONENTS}', components.join("\n"));
-  	template = template.replace('${PAGE_CONTENT}', pageContent);
-  	template = template.replace('${ENVIRONMENT}', environment);
-
-  	response = template;
-
-  	res.send(response);
-  }
+  // Create controller
+  var mainController = require('./lib/monitor/mainController');
+  mainController.configure(config);
 
   // Create index route
-  server.get('/', renderPageContent);
+  server.get('/', mainController.render);
 
   // Create content route
-  server.get('/environment/:environment', renderPageContent);
+  server.get('/environment/:environment', mainController.render);
 
   // Create statusOf route
   var statusOf = require('./lib/api/statusOf');
@@ -114,10 +43,8 @@ module.exports = function(config) {
   // Public API
   instance.server = server;
 
-  instance.listen = function()
-  {
-    // Starting listening for requests...
-    server.listen(serverPort, function() {
+  instance.listen = function() {
+    server.listen(config.serverPort, function() {
     	console.log(
     		'Product monitor available on ' +
     		url.format({
@@ -125,7 +52,7 @@ module.exports = function(config) {
     			hostname: 'localhost',
     			query: '',
     			pathname: '',
-    			port: serverPort
+    			port: config.serverPort
     		})
     	);
     })
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* Create a product monitor server with local content configuration */
 var monitor = require('./product-monitor');
 var server = monitor({
   "serverPort": 8080,
