989fd7c8e8e2c944fd9a55ccac6301286562dd3e
John Beech <john.beech@bbc.co.uk>
2015-06-08 23:33:31 +0100

    Added octo-credits for github contributors

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 /* Create a product monitor server with local content configuration */
 var monitor = require('./lib/product-monitor');
+
+var octoCreditsConfig = {
+  accessToken: require('./secret/github-access-token.js')
+}
+
 var server = monitor({
   "serverPort": 8080,
   "componentsPath": "monitoring/component/",
   "contentPath": "monitoring/content/",
   "templatesPath": "monitoring/templates/",
   "imagesPath": "images/",
-  "statusCacheTimeInSeconds": 60
+  "statusCacheTimeInSeconds": 60,
+  "octoCredits": octoCreditsConfig
 }).listen();
