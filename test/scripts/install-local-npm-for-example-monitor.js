a4e5f62703404a4fcd1db740bed476998ca07d5b
John Beech <github@mkv25.net>
2015-06-14 16:10:39 +0100

    Added firstTimeInstall script, with new proections around missing files.

--- test/scripts/install-local-npm-for-example-monitor.js
+++ test/scripts/install-local-npm-for-example-monitor.js
@@ -11,17 +11,33 @@ var packageJson = lazyLoad.json(moduleRoot + 'package.json', false);
 // Confirm that package.json has the expected properties
 assert.ok(packageJson, "Unable to find package.json");
 assert.ok(typeof packageJson.files === 'object', "No file list found on package.json");
+assert.ok(typeof packageJson.dependencies === 'object', "No dependencies found on package.json");
+
+function copyTo(source, destination, path) {
+  console.log("Copying path " + path);
+
+  ncp(source, destination, function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      console.log('Copied ' + path + ' done!');
+    }
+  });
+}
 
 // Copy files from module root, to example-monitor directory
 var files = packageJson.files;
 files.map(function(path) {
   var source = path;
   var destination = __dirname + '/../example-monitor/node_modules/product-monitor/' + path;
-  console.log("Copying path " + source + " to " + destination);
-  ncp(source, destination, function (err) {
-   if (err) {
-     return console.error(err);
-   }
-   console.log('done!');
-  });
+  copyTo(source, destination, path);
 });
+
+// Copy package dependencies from module root, to example-monitor directory
+var dependencies = packageJson.dependencies;
+for(path in dependencies) {
+  var source = "node_modules/" + path;
+  var destination = __dirname + '/../example-monitor/node_modules/product-monitor/node_modules/' + path;
+  copyTo(source, destination, path);
+};
