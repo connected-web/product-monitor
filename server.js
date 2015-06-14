53f56c47e23dd2decb19dd15e5d83acc16529e92
John Beech <github@mkv25.net>
2015-06-14 12:04:51 +0100

    Fixed lazyLoad module, added in lazyLoad.text(file, default) and lazyLoad.json(file, default)

--- server.js
+++ server.js
@@ -2,9 +2,7 @@
 var monitor = require('./lib/product-monitor');
 var lazyLoad = require('./lib/monitor/lazyLoad');
 
-var octoCreditsConfig = {
-  accessToken: lazyLoad('./secret/github-access-token.js', false)
-}
+var octoCreditsConfig = lazyLoad.json('./secret/github-access-token.json', { accessToken: false });
 
 var server = monitor({
   "serverPort": 8080,
